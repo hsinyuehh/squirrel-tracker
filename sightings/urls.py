95f7f2043c8d1cf3206c407e3c3b2850d7e41470
xinyue <xl2917@columbia.edu>
2019-12-06 18:37:40 -0500

    add urls for sightings

---
+++ sightings/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('',views.all_sightings),
+        path('add/',views.add_sighting),
+        path('<str:squirrel_id>/',views.sighting_details),
+        ]
