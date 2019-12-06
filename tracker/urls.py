0a6758aef7e81fdd2c6dbda1a3b777b8cc6fa90d
xinyue <xl2917@columbia.edu>
2019-12-06 18:45:19 -0500

    add map.html, modify tracker/urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from sightings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('sightings/', include('sightings.urls')),
+    path('map/', views.squirrel_map),
 ]
