81418cedb5221276f32217bf73f251dc1750e8ef
JamesJingrenYang <jy3026@columbia.edu>
2019-12-07 10:41:35 -0500

    Successfully export data to csv file

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -1,19 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
+from sightings.models import Squirrel
 import datetime
 import pandas as pd
-from sightings.models import Squirrel
 
 
 class Command(BaseCommand):
     help = 'Export Squirrel data to a csv file'
 
     def add_arguments(self, parser):
-        parser.add_argument('file_path', nargs='+', type=str, help="/path/to/file.csv")
+        parser.add_argument('path', nargs='+', type=str, help="/path/to/file.csv")
 
     def handle(self, *args, **options):
-        file_path = options['file_path'][0]
+        path = options['path'][0]
         df = pd.DataFrame(Squirrel.objects.all().values())
         df.replace('other', '', inplace=True)
         df['date'] = df['date'].apply(lambda x: x.strftime("%m%d%Y"))
-        df.to_csv(file_path, encoding='utf-8')
-        self.stdout.write(self.style.SUCCESS('Successfully export data to %s' % file_path))
+        df.to_csv(path)
+        self.stdout.write(self.style.SUCCESS(f'Successfully export data to {path}'))
