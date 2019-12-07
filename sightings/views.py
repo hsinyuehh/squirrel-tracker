b00f78dd444294e6863a6f53a205dd1b2ca21f23
JamesJingrenYang <jy3026@columbia.edu>
2019-12-07 12:21:29 -0500

    start create view stat

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,16 @@
+import pandas as pd
+import numpy as np
+import datetime
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Squirrel
 # Create your views here.
+
+
+
+def stats(request):
+    df=pd.DataFrame(Squirrel.objects.all().values())
+    temp = df['fur_color'].value_counts().values
+
+    return HttpResponse(f'The number of squirrels with different fur colors:<br>\
+     Gray: {temp[0]}; Cinnamon: {temp[1]}; Black: {temp[2]}; unknown: {temp[3]}')
